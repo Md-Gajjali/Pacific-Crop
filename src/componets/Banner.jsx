5f4c2a6e3877f69cbb576edbab5529c19b069898
Md-Gajjali <hellogajjali@gmail.com>
2025-12-28 16:04:47 +0600

    update banner part

--- src/componets/Banner.jsx
+++ src/componets/Banner.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import Button from '../componets/Button.jsx'
+import family from '../assets/family.png'
 
 const Banner = () => {
   return (
     <>
       <div className="container ">
         <div>
-            <h1 className='w-170 h-67.5 font-sansssa font-bold text-[70px] text-blackPrimary leading-o.2 mt-36 '>We Are Ready To Make Your New Life Happier.</h1>
-            <h3 className='mt-16 w-156.75 h-18 font-sansssa font-normal text-[20px] leading-9  '>We work with several leading insurance companies to secure insurance coverages tailored to meet your needs.</h3>
+          <h1
+            className='w-170 h-67.5 font-sansssa font-bold text-[70px] text-blackPrimary leading-o.2 mt-36 '>
+              We Are Ready To Make Your New Life Happier.
+          </h1>
+          <h3
+            className='mt-16 w-156.75 h-18 font-sansssa font-normal text-[20px] leading-9  '>
+              We work with several leading insurance companies to secure insurance coverages tailored to meet your needs.
+          </h3>
+          <Button>Get a consultation</Button>
         </div>
         <div>
-
+          <img src={family} alt="" />
         </div>
       </div>
     </>
