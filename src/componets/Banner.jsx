3479bb5ab901a7002e39ad3af1a135a60d6856ac
Md-Gajjali <hellogajjali@gmail.com>
2026-01-27 23:01:23 +0600

    update

--- src/componets/Banner.jsx
+++ src/componets/Banner.jsx
@@ -12,13 +12,13 @@ const Banner = () => {
             <div>
               <h1
                 className='w-170 h-67.5 font-sansssa font-bold text-[70px] text-heading leading-o.2 mt-36 '>
-                  We Are Ready To Make Your New Life Happier.
+                  We Are Ready To Make Your New Life Happier
               </h1>
               <p
                 className='py-16 text-paragraphColor mb-10 w-156.75 h-18 font-sansssa font-normal text-[20px] leading-9  '>
                   We work with several leading insurance companies to secure insurance coverages tailored to meet your needs.
               </p>
-              <Button>Get a consultation</Button>
+              <Button>Get a consultationn</Button>
             </div>
             <div>
               <img src={family} alt="banner" className='absolute top-11 right-0 ' />
