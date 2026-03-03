a0a4b6959d69725517e7f8aacb425e94e135e7a2
Md-Gajjali <hellogajjali@gmail.com>
2026-03-03 12:37:58 +0600

    update service card

--- src/componets/VerifiedCustomer.jsx
+++ src/componets/VerifiedCustomer.jsx
@@ -12,7 +12,7 @@ const VerifiedCustomer = () => {
             <div className='relative'>
                 <img src={img} alt=""  />
 
-            <div className='w-111.75 py-12 px-11.5 bg-white absolute top-10 left-12.75 rounded-2xl' >
+            <div className='w-111.75 py-12.5 px-11.25 bg-white absolute top-10 left-12.75 rounded-2xl' >
                 <img src={icon} alt="" />
                 <p className='font-sansssa font-semibold text-[25px] mt-5'>This platform is one of the best companies I have dealt with in Indonesia. They’re always happy to help, and i wouldn’t hesitate to recommend them.</p>
                 <h2 className='mt-10'>Daniel Anderson</h2>
