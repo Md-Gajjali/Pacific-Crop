24ffe6da6edc13da47b32b71189393dce5d17846
Md-Gajjali <hellogajjali@gmail.com>
2026-03-04 16:52:49 +0600

    update footer

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div className='mt-32'>
       <Container>
-        <Flex>
+        <Flex className='justify-between '>
           <div className='w-65.75'>
             <Flex className='items-center gap-2 '>
               <img src={pacific} alt="" />
@@ -18,45 +18,43 @@ const Footer = () => {
             <img src={group} alt="" className='mt-8' />
           </div>
           <div>
-            <h2>Product</h2>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+            <h2 className='text-[18px] font-bold'>Product</h2>
+            <ul className='mt-8 font-sansssa text-[16px] text-gray-500 leading-9'>
+              <li>Landingpage</li>
+              <li>Futures</li>
+              <li>Documentation</li>
+              <li>Referral program</li>
+              <li>Pricing</li>
             </ul>
           </div>
           <div>
-            <h2></h2>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+            <h2 className='text-[18px] font-bold'>Services</h2>
+            <ul className='mt-8 font-sansssa text-[16px] text-gray-500 leading-9'>
+              <li>Documentation</li>
+              <li>Design</li>
+              <li>Themes</li>
+              <li>Illustrations </li>
+              <li>UI Kit</li>
             </ul>
           </div>
           <div>
-            <h2></h2>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+            <h2 className='text-[18px] font-bold'>Company</h2>
+            <ul className='mt-8 font-sansssa text-[16px] text-gray-500 leading-9'>
+              <li>About</li>
+              <li>Terms</li>
+              <li>Privacy Policy</li>
+              <li>Careers</li>
             </ul>
           </div>
           <div>
-            <h2></h2>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+            <h2 className='text-[18px] font-bold'>More</h2>
+            <ul className='mt-8 font-sansssa text-[16px] text-gray-500 leading-9'>
+              <li>Documentation</li>
+              <li>License</li>
+              <li>Changelog</li>
             </ul>
           </div>
+
           
         </Flex>
       </Container>
