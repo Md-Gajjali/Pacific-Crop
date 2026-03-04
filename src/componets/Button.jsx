b0e8d151b9ff9c16000d05cd2abe27f4c66ac10a
Md-Gajjali <hellogajjali@gmail.com>
2026-03-04 16:29:32 +0600

    update emailpart

--- src/componets/Button.jsx
+++ src/componets/Button.jsx
@@ -1,9 +1,9 @@
 import React, { Children } from 'react'
 
-const Button = ({children}) => {
+const Button = ({children ,className}) => {
   return (
     <>
-      <button className='px-7.5 py-4 bg-primary cursor-pointer font-sansssa text-white text-[20px] rounded-[10px]'>
+      <button className='px-7.5 py-3 bg-primary cursor-pointer font-sansssa text-white text-[20px] rounded-[10px]'>
         {children}
       </button>
     </>
--- src/componets/EmailPart.jsx
+++ src/componets/EmailPart.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Container from './Container'
+import Flex from './Flex'
+import Button from './Button'
 
 const EmailPart = () => {
   return (
-    <div>
+    <div className='py-32.5 mt-32.5 bg-black'>
      <Container>
-        <h2 className='text-center text-[40px] '>Ready To Get Insured? Submit Your Request 
-And Hear Back As Soon As Tomorroe.</h2>
+        <h2 className='text-center text-[40px] text-white font-bold w-223.25 m-auto'>Ready To Get Insured? Submit Your Request 
+            And Hear Back As Soon As Tomorroe.</h2>
+        <Flex className='justify-center items-center mt-10 gap-5'>
+            <input type="text"  placeholder='Your Email' className='w-[320px] py-3 px-3 rounded-2xl bg-white '/>
+            <Button className='h-16.25 w-41.5 '>Get Started</Button>
+        </Flex>
+        <p className='text-white font-sansssa text-center w-138.75 m-auto mt-7'>Get a response tomorrow if you submit by 9pm today. If we received after 9pm will get a reponse the following day.</p>
      </Container>
     </div>
   )
